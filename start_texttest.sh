18a62d770849682ddec73a2d87f52c91abe661f6
Naimun Siraj <nsiraj@hubspot.com>
2024-11-02 11:56:38 -0400

    Initial setup

--- start_texttest.sh
+++ start_texttest.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
+# Check if virtual environment directory exists, and if not, create one using Python 3
 if [ ! -d "venv" ]; then
-    python -m venv venv
+    python3 -m venv venv
 fi
-venv/bin/pip install texttest
+
+# Activate the virtual environment
+. venv/bin/activate
+
+# Check if texttest is installed in the virtual environment
+if ! venv/bin/pip show texttest > /dev/null 2>&1; then
+    echo "texttest is not installed. Installing texttest..."
+    # Install texttest if it's not already installed
+    pip install git+https://github.com/texttest/texttest.git
+else
+    echo "texttest is already installed."
+fi
+
+# Run texttest
 venv/bin/texttest -d . -con "$@"
+
+# Deactivate virtual environment
+deactivate
